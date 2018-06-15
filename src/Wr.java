ff74cd5e756c5f15c8aa875156017631e6c2b476
Zaretskaya.es <liza.9999@bk.ru>
2018-06-15 11:20:50 +0300

    Fixin'

--- src/Wr.java
+++ src/Wr.java
@@ -1,24 +1,18 @@
-import javafx.scene.control.Alert;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Rectangle;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
 import javax.swing.*;
 
 public class Wr extends JPanel implements KeyListener,
         ActionListener, Runnable {
-    static boolean right = false;
-    static boolean left = false;
+    private static boolean right = false;
+    private static boolean left = false;
 
-    int posXGlobe = 160;
-    int posYGlobe = 218;
+    static int posXGlobe = 160;
+    static int posYGlobe = 218;
 
     int posXPlat = 160;
     int posYPlat = 245;
@@ -29,54 +23,59 @@ public class Wr extends JPanel implements KeyListener,
     int BlockWidth = 30;
     int BlockHeight = 20;
 
-    private static int points = 0;
-
-    Globe globe = new Globe(posXGlobe, posYGlobe, 10, 10);
-    Plat plat = new Plat(160, 245, 40, 5);
-    Block[] blocks = new Block[20];
+    static Globe globe = new Globe(posXGlobe, posYGlobe, 10, 10);
+    static Plat plat = new Plat(160, 245, 40, 5);
+    static Block[] blocks = new Block[20];
 
 
     int courseX = -1;
     int courseY = -1;
-    boolean fallingDown = false;
-    boolean blocksOver = false;
+    static boolean fallingDown = false;
+    static boolean blocksOver = false;
     int count = 0;
-    String text;
-    private static String heroName;
+    static String text;
+
+    public static class Main {
+
+        public static Painter painter = new Painter();
+
+        private static String heroName;
+        static int points = 0;
+
+        public static void main(String[] args) {
+            JFrame frame = new JFrame();
+            Wr game = new Wr();
+            JButton button = new JButton("RESTART");
+            frame.setSize(350, 305);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            frame.add(game);
+            frame.add(button, BorderLayout.SOUTH);
+            frame.setLocationRelativeTo(null);
+            frame.setResizable(false);
+            frame.setVisible(true);
+            button.addActionListener(game);
+
+            game.addKeyListener(game);
+            game.setFocusable(true);
+            Thread t = new Thread(game);
+
+            heroName = JOptionPane.showInputDialog(null, "Please enter your name:",
+                    "Wrecking Ball", JOptionPane.QUESTION_MESSAGE);
+            if (heroName == null) {
+                System.exit(0);
+            }
+            if (heroName.toUpperCase().equals("LZ") || heroName.equals("LizaZaretskaya")
+                    || heroName.equals("Liza") || heroName.toUpperCase().equals("PM")) {
+                points += 50;
+                JOptionPane.showMessageDialog(null, "You got secret 50 points",
+                        "50 Points", JOptionPane.INFORMATION_MESSAGE);
+
+            }
 
 
-
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        Wr game = new Wr();
-        JButton button = new JButton("RESTART");
-        frame.setSize(350, 400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        frame.add(game);
-        frame.add(button, BorderLayout.SOUTH);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setVisible(true);
-        button.addActionListener(game);
-
-        game.addKeyListener(game);
-        game.setFocusable(true);
-        Thread t = new Thread(game);
-
-        heroName = JOptionPane.showInputDialog(null, "Please enter your name:",
-                "Wrecking Ball", JOptionPane.QUESTION_MESSAGE);
-        if (heroName == null) {
-            System.exit(0);
+            t.start();
         }
-        if (heroName.toUpperCase().equals("LZ") || heroName.equals("LizaZaretskaya")
-                || heroName.equals("Liza") || heroName.toUpperCase().equals("PM")) {
-            points += 50;
-            JOptionPane.showMessageDialog(null, "You got secret 50 points",
-                    "50 Points", JOptionPane.INFORMATION_MESSAGE);
-
-        }
-        t.start();
     }
 
 
@@ -85,20 +84,20 @@ public class Wr extends JPanel implements KeyListener,
         g.setColor(Color.LIGHT_GRAY);
         g.fillRect(0, 0, 350, 450);
         g.setColor(Color.white);
-        g.setFont(new Font("Times New Roman",Font.BOLD,15));
-        g.drawString("Points: "+points,260,20);
+        g.setFont(new Font("Times New Roman", Font.BOLD, 15));
+        g.drawString("Points: " + Main.points, 260, 20);
         g.setColor(Color.blue);
         g.fillOval(globe.x, globe.y, globe.width, globe.height);
         g.setColor(Color.black);
         g.fill3DRect(plat.x, plat.y, plat.width, plat.height, true);
-        g.setColor(Color.GRAY);
-        g.fillRect(0, 251, 450, 200);
+        //g.setColor(Color.GRAY);
+        //g.fillRect(0, 251, 450, 200);
         g.setColor(Color.black);
         g.drawRect(0, 0, 343, 250);
-        for (int i = 0; i < blocks.length; i++) {
-            if (blocks[i] != null) {
-                g.fill3DRect(blocks[i].getBlockX(), blocks[i].getBlockY(), blocks[i].getBlockWidth(),
-                        blocks[i].getBlockHeight(), true);
+        for (Block block : blocks) {
+            if (block != null) {
+                g.fill3DRect(block.getBlockX(), block.getBlockY(), block.getBlockWidth(),
+                        block.getBlockHeight(), true);
             }
         }
 
@@ -123,7 +122,7 @@ public class Wr extends JPanel implements KeyListener,
                     if (blocks[i].intersects(globe)) {
                         blocks[i] = null;
                         courseY = -courseY;
-                        points += 25;
+                        Main.points += 25;
                         count++;
                     }
                 }
@@ -131,28 +130,23 @@ public class Wr extends JPanel implements KeyListener,
 
             if (count == blocks.length) {
                 blocksOver = true;
-                /*
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setHeaderText("Winner");
-                alert.setTitle("YOU WON THE GAME! POINTS: "+points);
-                alert.showAndWait();
-                System.exit(1);
-                */
-                text = "YOU WON THE GAME! POINTS:"+points;
+                text = "YOU WON THE GAME! POINTS: " + Main.points;
                 repaint();
-                break;
+
+                courseY = 0;
+                courseX = 0;
             }
 
             repaint();
             globe.x += courseX;
             globe.y += courseY;
 
-            if (left == true) {
-                plat.x-=3;
+            if (left) {
+                plat.x -= 3;
                 right = false;
             }
-            if (right == true) {
-                plat.x+=3;
+            if (right) {
+                plat.x += 3;
                 left = false;
             }
             if (plat.x <= 4) {
@@ -160,11 +154,9 @@ public class Wr extends JPanel implements KeyListener,
             } else if (plat.x >= 298) {
                 plat.x = 298;
             }
-
             if (plat.intersects(globe)) {
                 courseY = -courseY;
             }
-
             if (globe.x <= 0 || globe.x + globe.height >= 343) {
                 courseX = -courseX;
             }
@@ -173,7 +165,7 @@ public class Wr extends JPanel implements KeyListener,
             }
             if (globe.y >= 250) {
                 fallingDown = true;
-                text = "YOU LOST THE GAME! POINTS: "+points;
+                text = "YOU LOST THE GAME! POINTS: " + Main.points;
                 repaint();
 
             }
@@ -227,10 +219,11 @@ public class Wr extends JPanel implements KeyListener,
         initializeVariables();
         createBlocks();
         repaint();
-        points = 0;
+        Main.points = 0;
+
     }
 
-    public void initializeVariables(){
+    public void initializeVariables() {
 
         posXGlobe = 160;
         posYGlobe = 218;
@@ -275,5 +268,4 @@ public class Wr extends JPanel implements KeyListener,
             BlockX += (BlockWidth + 3);
         }
     }
-
 }
